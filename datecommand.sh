c3c01372dac8e1532d82f65fcc36bd5d76d6e1dd
ivyflammenwald <36801451+ivyflammenwald@users.noreply.github.com>
2018-03-09 05:17:04 -0600

    Add files via upload
    primer commit de fecha: se crea la funcion que devuelve la fecha usando el comando date, se usan diferentes variables para almacenar los valores de la fecha, se imprimen con formato acomodando las variables en el echo

---
+++ datecommand.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+F=`date +"%d:%m:%Y"`
+dia=`date +"%d"`
+mes=`date +"%m"`
+anio=`date +"%Y"`
+diad=`date +"%A"`
+mesn=`date +"%B"`
+
+
+echo "hoy es $diad $dia de $mesn ($mes) del año $anio"
+echo "o sea hoy es $F"
+
+#../main.sh
+
+#dia, mes, anio son resultados numericos y diad,mesn 
+#son los nombres, se imprime en formato cool de oracion
