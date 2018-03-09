e7e108acb7524e77d5a0cd055c2258a7f0496f4d
ivyflammenwald <36801451+ivyflammenwald@users.noreply.github.com>
2018-03-09 05:26:49 -0600

    se hacen las modificaciones para rtc, comando listo
    2do commit comando hora: 
    se agrega el trap para no poder salir de con ctrl c, se cambia al comando rtc para obtener la hora (se lista todo el comando con cat); en vez de buscar por atributos se selecciona una impresión completa y en formato utilizando egrep, se agregan colores a la funcion

--- Impre/hourcommand.sh
+++ Impre/hourcommand.sh
@@ -1,23 +1,14 @@
 #!/bin/bash
+trap "echo ' No puedes interrumpir la ejecucion mediante este comando' " INT
 
-Hora=`date +"%H:%M"`
-hour=`date +"%k"`
-min=`date +"%M"`
-dedia=19
-manana=12
+azul="\e[36m"
+normal="\e[0m"
 
-if test $hour -lt $manana
-	then
-		echo "son las $hour de la manana con $min minutos"
+echo "prebeshell dice -"
+echo -n -e "$azul la hora es: "
 
-		elif  [ $hour -gt $manana ] && [ $hour -lt $dedia ]
-			then
-			echo "son las $hour de la tarde con $min minutos"
-		else 
-			echo "son las $hour de la noche con $min minutos"
-fi
+cat /proc/driver/rtc | grep "rtc_time*" | grep "[0-9]*:[0-9]*:[0-9]*" -o #el grep delimita la impresión del cat para visualizar unicamente la fecha
 
-
-echo "ya es hora prros, son las $hour "
+echo -e "$normal el formato es de 24 horas \n"
 
 #../main.sh
